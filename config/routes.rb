74f9f5358c9f39e9ff234cbf5fc3c1799f772607
Kamil Matyska <git@matyska.org>
2013-07-13 12:01:49 -0700

    Connected Vote to Topic and enabled voting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Suggestotron::Application.routes.draw do
+  resources :votes
+
+  resources :topics
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
